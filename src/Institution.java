4a5103675fb45ad3f1336649477a925dd7dec72e
JealousOne <nurzhanzeynulla2006@gmail.com>
2025-12-26 12:18:52 +0500

    commit

---
+++ src/Institution.java
@@ -0,0 +1,29 @@
+class Institution {
+    private String name;
+    private String city;
+
+    public Institution(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getInfo() {
+        return "Institution: Name: " + name + ", City: " + city + ".";
+    }
+}
---
+++ src/Teacher.java
@@ -0,0 +1,39 @@
+class Teacher {
+    private int id;
+    private String name;
+    private String subject;
+
+    public Teacher(int id, String name, String subject) {
+        this.id = id;
+        this.name = name;
+        this.subject = subject;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getInfo() {
+        return "Teacher: Name: " + name + ", Subject: " + subject + ".";
+    }
+}
